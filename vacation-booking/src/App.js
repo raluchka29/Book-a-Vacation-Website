443635d3162e49209a55f8dc95baca8d4fb42244
Raluca <raluca.elena.butacu@gmail.com>
2024-04-10 14:06:22 +0300

    TopBar + Starting Navbar

---
+++ vacation-booking/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import TopBar from "./components/TopBar";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div>
+      <TopBar />
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
