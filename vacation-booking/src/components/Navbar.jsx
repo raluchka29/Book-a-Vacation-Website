6cdb316083f94db05d8887ab88e156eeb3fe67c1
Raluca <raluca.elena.butacu@gmail.com>
2024-04-11 22:18:11 +0300

    navbar updated

--- vacation-booking/src/components/Navbar.jsx
+++ vacation-booking/src/components/Navbar.jsx
@@ -1,18 +1,23 @@
 import React, {useState, useEffect} from "react";
-import { FaBars } from "react-icons/fa";
 import { BsChatSquareDots } from "react-icons/bs";
 import {
     FaFacebookF,
     FaTwitter,
     FaGooglePlusG,
     FaInstagram,
+    FaBars,
 } from "react-icons/fa";
 
 const Navbar = () => {
+const[nav, setNav] = useState(false)
+const handleNav =() => {
+    setNav(!nav)
+}
+
 return (
-    <div>
+    <div className="w-full min-h-[50px] flex justify-between items-center absolute z-10 text-white bg-gray-700/80">
         <ul>
-            <li>
+            <li className="hidden sm:flex px-4">
                 <a href="/">Home</a>
             </li>
             <li>
@@ -25,6 +30,33 @@ return (
                 <a href="#contact">Contact</a>
             </li>
         </ul>
+        <div className="flex justify-between">
+            <FaFacebookF className="mx-4" />            
+            <FaTwitter className="mx-4" />
+            <FaGooglePlusG className="mx-4" />
+            <FaInstagram className="mx-4" />
+        </div>
+        {/*Hamburger icon*/}
+        <div className="sm:hidden z-10" onClick={handleNav}>
+            <FaBars size={20} className="mr-4 cursor-pointer"/>
+        </div>
+        {/*Mobile Menu*/}
+        <div onClick={handleNav} className={nav ? 'overflow-y md:hidden ease-in duration-300 absolute text-gray-300 lef-0 top-0 w-full h-screen bg-black/90 px-4 py-7 flex flex-col': 'absolute top-0 h-screen left-[-100%] ease-in duration-500'}>
+            <ul className="h-full w-full text-center pt-12">
+                <li className="text-2xl py-8">
+                <a href="/">Home</a>
+            </li>
+            <li className="text-2xl py-8">
+                <a href="#gallery">Gallery</a>
+            </li>
+            <li className="text-2xl py-8">
+                <a href="#deals">Deals</a>
+            </li>
+            <li className="text-2xl py-8">
+                <a href="#contact">Contact</a>
+            </li>
+            </ul>
+        </div>
     </div>
 )
 }
